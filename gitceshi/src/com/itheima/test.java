32ced262ebb0c50c4c62961e4d1dde699dcd6d67
binliang <qyq520@163.com>
2018-12-16 20:32:48 +0800

    修改

--- gitceshi/src/com/itheima/test.java
+++ gitceshi/src/com/itheima/test.java
@@ -2,6 +2,11 @@ package com.itheima;
 
 public class test {
     public static void main(String[] args) {
+
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+
         System.out.println("niasdasd");
     }
 }
